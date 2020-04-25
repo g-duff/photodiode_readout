2bcf197271a66199d298f90396abcd1359d6155f
George Duffett <50420181+g-duff@users.noreply.github.com>
2020-04-25 23:44:18 +0200

    Added repeats

--- arduino_scripts/on_off/on_off.ino
+++ arduino_scripts/on_off/on_off.ino
@@ -2,6 +2,7 @@
 #define LED645 3
 #define LED660 6
 #define inPin 0
+int repeats = 3;
 
 void setup()
 {
@@ -19,36 +20,45 @@ void loop()
     String outspace = " ";
     bool LED645status;
     bool LED660status;
+    int i;
 
     // 645nm LED
     LED645status = true;
     digitalWrite(LED645, LED645status);
     delay(delayTime);
     
-    brightout = analogRead(inPin);
-    Serial.println(LED645status + outspace + LED660status + outspace + brightout);
+    for (i=0; i<repeats; i++) {
+        brightout = analogRead(inPin);
+        Serial.println(LED645status + outspace + LED660status + outspace + brightout);
+    }
 
+    // Off
     LED645status = false;
     digitalWrite(LED645, LED645status);
     delay(delayTime);
 
-    // Off
-    brightout = analogRead(inPin);
-    Serial.println(LED645status + outspace + LED660status + outspace + brightout);
+    for (i=0; i<repeats; i++) {
+        brightout = analogRead(inPin);
+        Serial.println(LED645status + outspace + LED660status + outspace + brightout);
+    }
 
     // 660nm LED
     LED660status = true;
     digitalWrite(LED660, LED660status);
     delay(delayTime);
 
-    brightout = analogRead(inPin);
-    Serial.println(LED645status + outspace + LED660status + outspace + brightout);
+    for (i=0; i<repeats; i++) {
+        brightout = analogRead(inPin);
+        Serial.println(LED645status + outspace + LED660status + outspace + brightout);
+    }
 
+    // Off
     LED660status = false;
     digitalWrite(LED660, LED660status);
     delay(delayTime);
 
-    // Off
-    brightout = analogRead(inPin);
-    Serial.println(LED645status + outspace + LED660status + outspace + brightout);
+    for (i=0; i<repeats; i++) {
+        brightout = analogRead(inPin);
+        Serial.println(LED645status + outspace + LED660status + outspace + brightout);
+    }
 }
