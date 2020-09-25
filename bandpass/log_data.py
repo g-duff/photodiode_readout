51cab96ec15bb589cd8813d882c32abbae6373be
George Duffett <50420181+g-duff@users.noreply.github.com>
2020-09-25 16:43:57 +0200

    Updated filesaving

--- bandpass/log_data.py
+++ bandpass/log_data.py
@@ -2,11 +2,13 @@ import serial
 import matplotlib.pyplot as plt
 import numpy as np
 
-
-ser = serial.Serial('COM3', timeout=1, baudrate=115200)
+ser = serial.Serial('COM5', timeout=3, baudrate=115200)
 i = 0
 
 while True:
     l = ser.readline()
-    np.savetxt(l.decode(), f'{i}.csv')
+    l = l.decode().split(' ')
+    decoded = np.array(l[:-1], dtype=np.float)
+    np.savetxt(f'./data/{i}.csv', decoded)
     i += 1
+    print(i)
