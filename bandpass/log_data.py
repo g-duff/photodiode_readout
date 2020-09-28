517c402c903e40d6608b9700964828673f5ab38e
George Duffett <george.duffett@me.com>
2020-09-28 13:26:27 +0100

    Updated data logging

--- bandpass/log_data.py
+++ bandpass/log_data.py
@@ -1,14 +1,17 @@
 import serial
-import matplotlib.pyplot as plt
 import numpy as np
+from datetime import datetime
 
-ser = serial.Serial('COM5', timeout=3, baudrate=115200)
+
+ser = serial.Serial('/dev/cu.usbmodem14301', timeout=3, baudrate=115200)
+# ser = serial.Serial('COM5', timeout=3, baudrate=115200)
 i = 0
 
 while True:
     l = ser.readline()
     l = l.decode().split(' ')
+    now = datetime.now()
     decoded = np.array(l[:-1], dtype=np.float)
-    np.savetxt(f'./data/{i}.csv', decoded)
+    np.savetxt(f'./data/{i:06d}.csv', decoded, header=str(datetime.now()))
     i += 1
     print(i)
--- bandpass/process_bandpass.py
+++ bandpass/process_bandpass.py
@@ -1,27 +1,45 @@
-import matplotlib.pyplot as plt
 import numpy as np
 import numpy.fft as fft
+import matplotlib
+import matplotlib.pyplot as plt
 
-fname = '499'
-infile = np.loadtxt(f'./{fname}.csv')
+font = {'size': 14}
+matplotlib.rc('font', **font)
 
-for i in range((infile.shape)[0]):
-    vt = infile[i,:]
-    vf = fft.fft(vt)
+infiles = [np.loadtxt(f"./data/{i:06d}.csv", skiprows=1) for i in range(10)]
 
-    dt = 0.002
-    n = len(vt)
+# DC signal
+dc = np.array([i[3] for i in infiles])
 
-    # t = np.arange(0, n*dt, dt)
-    f = fft.fftfreq(n=n, d=dt)
-    # vf[0] = 0
+# Mean signal
+me = np.array([2*np.mean(i) for i in infiles])
 
-    vf = fft.fftshift(vf)/n
-    f = fft.fftshift(f)
+# Bandpass filter
+n = len(infiles[0])
+l = int(n/2)
+fr = [fft.fft(i) for i in infiles]
+fr = [np.concatenate((
+    [vf[0]],
+    (vf[1:l]+vf[:l+1:-1]))) for vf in fr]
+fr = np.array([4*abs(f[80])/n for f in fr])
 
-    fig, ax = plt.subplots()
-    ax.plot(f, abs(vf), ms=5)
-    ax.set_xlim([-210, 210])
-    plt.show()
-    # fig.savefig(f'{fname}_{i}.png')
-    # plt.close('all')
+fig, (ax1, ax2, ax3) = plt.subplots(nrows=3, sharex=True)
+ax1.plot(dc[1::2], 'C0.', ms=4)
+ax2.plot(me[1::2], 'C1.', ms=4)
+ax3.plot(fr[1::2], 'C2.', ms=4)
+fig.savefig('shift_time.png')
+plt.show()
+
+# vt = infiles[0][1:]
+# vf = fft.fft(vt)
+# dt = 0.001
+# n = len(vt)
+# # t = np.arange(0, n*dt, dt)
+# f = fft.fftfreq(n=n, d=dt)
+# # vf[0] = 0
+# vf = fft.fftshift(vf)/n
+# f = fft.fftshift(f)
+# fig, ax = plt.subplots()
+# ax.plot(f, abs(vf), ms=5)
+# ax.set_xlim([-210, 210])
+# plt.show()
